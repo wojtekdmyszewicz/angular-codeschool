7e785ed0974bd40642f7ae500f5ea127eee5f720
Wojtek Dmyszewicz <wdmy85@gmail.com>
2014-10-19 13:56:28 +0200

    added filters added images

---
+++ NOTES.md
@@ -0,0 +1,8 @@
+http://streaming.radionomy.com/A-JAZZ-FM-WEB
+
+http://127.0.0.1:3001/
+
+https://github.com/mgechev/angularjs-style-guide
+http://campus.codeschool.com/courses/shaping-up-with-angular-js/level/2/section/1/video/1
+
+http://flickholdr.iwerk.org/
--- app/app.js
+++ app/app.js
@@ -3,22 +3,46 @@
 
     app.controller('StoreController', function(){
         this.products = gems;
+
+        // TODO: reload image source
     });
 
     var gems = [
         {
             name: 'Product',
-            price: 2.95,
+            price: 2,
+            date: '1227201312501',
             description: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla.',
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            images: [
+                {
+                  full: 'http://flickholdr.iwerk.org/400/400',
+                  thumb: 'http://flickholdr.iwerk.org/200/200'
+                },
+                {
+                  full: 'http://flickholdr.iwerk.org/400/400',
+                  thumb: 'http://flickholdr.iwerk.org/200/200'
+                }
+            ]
         },
         {
             name: 'Product',
             price: 2.95,
+            date: '1227201312501',
             description: 'Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec ullamcorper nulla non metus auctor fringilla.',
             canPurchase: true,
-            soldOut: false
+            soldOut: false,
+            images: [
+                {
+                    full: 'http://flickholdr.iwerk.org/400/400',
+                    thumb: 'http://flickholdr.iwerk.org/200/200'
+                },
+                {
+                    full: 'http://flickholdr.iwerk.org/400/400',
+                    thumb: 'http://flickholdr.iwerk.org/200/200'
+                }
+            ]
         }
     ];
 
